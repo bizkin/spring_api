26fb0f504c8571bee34d3d86d6d05e29132787e8
Tal Litvak <litvaktal04@gmail.com>
2019-12-08 18:17:19 +0200

    ok, everything seems to work!

--- src/main/java/com/example/restapi/DemoApplication.java
+++ src/main/java/com/example/restapi/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("This is Why VSCODE is the best!");
+		System.out.println("This is Why VSCODE is the best! testing after commited file");
 	}
 
 }
